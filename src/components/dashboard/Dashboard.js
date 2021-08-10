2fc781d9055f2e155d2598a284e234e5a430453a
Dave Murray <dave@ohmnid.com>
2021-08-10 09:11:44 +0100

    changes to files caused by Prettier correctly formatting white space and indents

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,35 +1,40 @@
-
 const stats = [
-    { name: 'Total Subscribers', stat: '71,897' },
-    { name: 'Avg. Open Rate', stat: '58.16%' },
-    { name: 'Avg. Click Rate', stat: '24.57%' },
-  ]
-  
-  export default function Dashboard() {
-    return (
+	{ name: "Total Subscribers", stat: "71,897" },
+	{ name: "Avg. Open Rate", stat: "58.16%" },
+	{ name: "Avg. Click Rate", stat: "24.57%" },
+];
 
-        
-      
-      <div>
-        <header>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h1 className="text-3xl font-bold leading-tight text-gray-900">Dashboard</h1>
-        </div>
-        
-      </header>
+export default function Dashboard() {
+	return (
+		<div>
+			<header>
+				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+					<h1 className="text-3xl font-bold leading-tight text-gray-900">
+						Dashboard
+					</h1>
+				</div>
+			</header>
 
-      <main className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">Last 30 days</h3>
-        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          {stats.map((item) => (
-            <div key={item.name} className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-              <dt className="text-sm font-medium text-gray-500 truncate">{item.name}</dt>
-              <dd className="mt-1 text-3xl font-semibold text-gray-900">{item.stat}</dd>
-            </div>
-          ))}
-        </dl>
-        </main>
-      </div>
-    )
-  }
-  
+			<main className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+				<h3 className="text-lg leading-6 font-medium text-gray-900">
+					Last 30 days
+				</h3>
+				<dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+					{stats.map((item) => (
+						<div
+							key={item.name}
+							className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6"
+						>
+							<dt className="text-sm font-medium text-gray-500 truncate">
+								{item.name}
+							</dt>
+							<dd className="mt-1 text-3xl font-semibold text-gray-900">
+								{item.stat}
+							</dd>
+						</div>
+					))}
+				</dl>
+			</main>
+		</div>
+	);
+}
--- src/containers/shell/Main.js
+++ src/containers/shell/Main.js
@@ -1,20 +1,19 @@
-import { Route } from 'react-router'
-import Navbar from './Navbar'
-import Dashboard from '../../components/dashboard/Dashboard'
-import Artworks from '../../components/artworks/Artworks'
+import { Route } from "react-router";
+import Navbar from "./Navbar";
+import Dashboard from "../../components/dashboard/Dashboard";
+import Artworks from "../../components/artworks/Artworks";
 
-export default function Main() {
-  return (
-    <div>
-        <nav>
-          <Navbar />
-        </nav>
-      
-      <div className="py-10">
+export default function Main({ collection }) {
+	return (
+		<div>
+			<nav>
+				<Navbar collection={collection} />
+			</nav>
 
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/artworks" component={Artworks} />     
-      </div>
-    </div>
-  )
+			<div className="py-10">
+				<Route path="/dashboard" component={Dashboard} />
+				<Route path="/artworks" component={Artworks} />
+			</div>
+		</div>
+	);
 }
