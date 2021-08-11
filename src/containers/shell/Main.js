452f249b452772865db78a1f0b5d735abaaf4caf
OhmniD <dave@ohmnid.com>
2021-08-11 21:17:57 +0100

    makes collection context provider separate - tested and working

--- src/containers/shell/Main.js
+++ src/containers/shell/Main.js
@@ -3,19 +3,22 @@ import Navbar from "./Navbar";
 import Dashboard from "../../components/dashboard/Dashboard";
 import Artworks from "../../components/artworks/Artworks";
 import Artwork from "../../components/artworks/Artwork";
+import { CollectionProvider } from "../../providers/CollectionProvider";
 
 export default function Main({ collection }) {
-  return (
-    <div>
-      <nav>
-        <Navbar collection={collection} />
-      </nav>
+	return (
+		<div>
+			<nav>
+				<Navbar collection={collection} />
+			</nav>
 
-      <div className="py-10 px-10">
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/artworks" component={Artworks} />
-        <Route path="/artwork" render={(props) => <Artwork {...props} />} />
-      </div>
-    </div>
-  );
+			<div className="py-10 px-10">
+				<CollectionProvider>
+					<Route path="/" exact component={Dashboard} />
+					<Route path="/artworks" component={Artworks} />
+					<Route path="/artwork" render={(props) => <Artwork {...props} />} />
+				</CollectionProvider>
+			</div>
+		</div>
+	);
 }
---
+++ src/providers/CollectionProvider.js
@@ -0,0 +1,41 @@
+import { useState, createContext, useEffect } from "react";
+import { Auth, API, graphqlOperation } from "aws-amplify";
+import { createCollection } from "../graphql/mutations";
+import { listCollections } from "../graphql/queries";
+
+export const CollectionContext = createContext();
+
+export const CollectionProvider = (props) => {
+	const [collection, setCollection] = useState([]);
+
+	const getCollection = async () => {
+		const owner = await Auth.currentAuthenticatedUser();
+		const filter = { owner: { eq: owner } };
+
+		const collection = await API.graphql(graphqlOperation(listCollections), {
+			variables: { filter: filter },
+		});
+
+		if (collection.data.listCollections.scannedCount === 0) {
+			//Test this with a new user!
+			const newCollection = makeCollection();
+			setCollection(newCollection.data.listCollections.items[0]);
+		} else {
+			setCollection(collection.data.listCollections.items[0]);
+		}
+	};
+
+	const makeCollection = async () => {
+		await API.graphql(graphqlOperation(createCollection, { input: {} }));
+	};
+
+	useEffect(() => {
+		getCollection();
+	}, []);
+
+	return (
+		<CollectionContext.Provider value={{ collection }}>
+			{props.children}
+		</CollectionContext.Provider>
+	);
+};
