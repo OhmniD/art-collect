dd19b461e14f01005915fdb2b3b7dc42b53b1432
OhmniD <dave@ohmnid.com>
2021-08-11 17:33:43 +0100

    removes some unused imports and variables

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,48 +1,44 @@
-import { useContext, useEffect, useState } from 'react'
-import { CollectionContext } from '../../App'
-import { Medium, Artwork, Collection, Artist } from "../../models";
-import DashboardStats from './DashboardStats';
+import { useContext, useEffect, useState } from "react";
+import { CollectionContext } from "../../App";
+import DashboardStats from "./DashboardStats";
 
 export default function Dashboard() {
-	const {collection, mediums, artists, artworks, isLoaded} = useContext(CollectionContext);
+	const { mediums, artists, artworks, isLoaded } =
+		useContext(CollectionContext);
 
-	const [dashboardNodes, setDashboardNodes] = useState([])
+	const [dashboardNodes, setDashboardNodes] = useState([]);
 
 	const getCounts = () => {
 		const counts = [
-			{name: "Total Artworks", stat: artworks.length},
-			{name: "Total Artists", stat: artists.length},
-			{name: "Total Mediums", stat: mediums.length}
-		]
+			{ name: "Total Artworks", stat: artworks.length },
+			{ name: "Total Artists", stat: artists.length },
+			{ name: "Total Mediums", stat: mediums.length },
+		];
 		return counts.map((item, index) => {
-			return <DashboardStats key={index} name={item.name} stat={item.stat} />
-		})
-
-		
-	}
+			return <DashboardStats key={index} name={item.name} stat={item.stat} />;
+		});
+	};
 
 	useEffect(() => {
-		if (isLoaded !== false){
-			const dashboardNodes = getCounts()
-			setDashboardNodes(dashboardNodes)
+		if (isLoaded !== false) {
+			const dashboardNodes = getCounts();
+			setDashboardNodes(dashboardNodes);
 		}
-	}, [isLoaded])
+	}, [isLoaded]);
 
 	return (
-
 		<div>
 			<header>
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 					<h1 className="text-3xl font-bold leading-tight text-gray-900">
 						Dashboard
 					</h1>
-				
 				</div>
 			</header>
 			<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-			<dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-			{dashboardNodes}
-			</dl>
+				<dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+					{dashboardNodes}
+				</dl>
 			</div>
 		</div>
 	);
--- src/components/dashboard/DashboardStats.js
+++ src/components/dashboard/DashboardStats.js
@@ -1,17 +1,13 @@
-const DashboardStats = ({key, name, stat}) => {
-    return (
-                <div
-                    key={key}
-                    className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-                    <dt className="text-sm font-medium text-gray-500 truncate">
-                        {name}
-                    </dt>
-                    <dd className="mt-1 text-3xl font-semibold text-gray-900">
-                        {stat}
-                    </dd>
-                </div>
-
-    )
-}
+const DashboardStats = ({ name, stat }) => {
+	return (
+		<div
+			key={name}
+			className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6"
+		>
+			<dt className="text-sm font-medium text-gray-500 truncate">{name}</dt>
+			<dd className="mt-1 text-3xl font-semibold text-gray-900">{stat}</dd>
+		</div>
+	);
+};
 
 export default DashboardStats;
