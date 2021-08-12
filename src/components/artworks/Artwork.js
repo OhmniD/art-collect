bd23467945097b88af56190f12a6625de32ddbda
Dave Murray <dave@ohmnid.com>
2021-08-12 14:48:51 +0100

    artwork details now being passed down as props from list

--- src/components/artworks/Artwork.js
+++ src/components/artworks/Artwork.js
@@ -1,70 +1,75 @@
+import { useLocation } from "react-router";
+
 import {
-	CreditCardIcon,
-	KeyIcon,
-	UserCircleIcon,
-	UserGroupIcon,
-	ViewGridAddIcon,
+  CreditCardIcon,
+  KeyIcon,
+  UserCircleIcon,
+  UserGroupIcon,
+  ViewGridAddIcon,
 } from "@heroicons/react/outline";
 
 const navigation = [
-	{ name: "Artwork Details", href: "#", icon: UserCircleIcon, current: true },
-	{ name: "Images", href: "#", icon: KeyIcon, current: false },
-	{
-		name: "Loans & Consignment",
-		href: "#",
-		icon: CreditCardIcon,
-		current: false,
-	},
-	{ name: "Provenance", href: "#", icon: UserGroupIcon, current: false },
-	{
-		name: "Location & Shipping",
-		href: "#",
-		icon: ViewGridAddIcon,
-		current: false,
-	},
-	{ name: "Financial", href: "#", icon: ViewGridAddIcon, current: false },
-	{ name: "Notes", href: "#", icon: ViewGridAddIcon, current: false },
+  { name: "Artwork Details", href: "#", icon: UserCircleIcon, current: true },
+  { name: "Images", href: "#", icon: KeyIcon, current: false },
+  {
+    name: "Loans & Consignment",
+    href: "#",
+    icon: CreditCardIcon,
+    current: false,
+  },
+  { name: "Provenance", href: "#", icon: UserGroupIcon, current: false },
+  {
+    name: "Location & Shipping",
+    href: "#",
+    icon: ViewGridAddIcon,
+    current: false,
+  },
+  { name: "Financial", href: "#", icon: ViewGridAddIcon, current: false },
+  { name: "Notes", href: "#", icon: ViewGridAddIcon, current: false },
 ];
 
 function classNames(...classes) {
-	return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Artwork() {
-	return (
-		<div className="lg:grid lg:grid-cols-12 lg:gap-x-5">
-			<aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
-				<nav className="space-y-1">
-					{navigation.map((item) => (
-						<a
-							key={item.name}
-							href={item.href}
-							className={classNames(
-								item.current
-									? "bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white"
-									: "text-gray-900 hover:text-gray-900 hover:bg-gray-50",
-								"group rounded-md px-3 py-2 flex items-center text-sm font-medium"
-							)}
-							aria-current={item.current ? "page" : undefined}
-						>
-							<item.icon
-								className={classNames(
-									item.current
-										? "text-indigo-500 group-hover:text-indigo-500"
-										: "text-gray-400 group-hover:text-gray-500",
-									"flex-shrink-0 -ml-1 mr-3 h-6 w-6"
-								)}
-								aria-hidden="true"
-							/>
-							<span className="truncate">{item.name}</span>
-						</a>
-					))}
-				</nav>
-			</aside>
+  const { artwork, artist, medium } = useLocation().state;
+  return (
+    <div className="lg:grid lg:grid-cols-12 lg:gap-x-5">
+      <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
+        <nav className="space-y-1">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className={classNames(
+                item.current
+                  ? "bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white"
+                  : "text-gray-900 hover:text-gray-900 hover:bg-gray-50",
+                "group rounded-md px-3 py-2 flex items-center text-sm font-medium"
+              )}
+              aria-current={item.current ? "page" : undefined}
+            >
+              <item.icon
+                className={classNames(
+                  item.current
+                    ? "text-indigo-500 group-hover:text-indigo-500"
+                    : "text-gray-400 group-hover:text-gray-500",
+                  "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+                )}
+                aria-hidden="true"
+              />
+              <span className="truncate">{item.name}</span>
+            </a>
+          ))}
+        </nav>
+      </aside>
 
-			<div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
-				<h1>Hello world</h1>
-			</div>
-		</div>
-	);
+      <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
+        <h1>{artwork.title}</h1>
+        <h2>{artist.name}</h2>
+        <h3>{medium.medium}</h3>
+      </div>
+    </div>
+  );
 }
--- src/components/artworks/ArtworkListView.js
+++ src/components/artworks/ArtworkListView.js
@@ -1,41 +1,49 @@
-import { withRouter, Link } from "react-router";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
 const ArtworkListView = ({ key, artwork, artist, medium, history }) => {
-	const handleNavClick = (page) => (event) => history.push(page);
+  // const handleNavClick = (page) => (event) => history.push(page);
 
-	return (
-		<tr key={key} onClick={handleNavClick("/artwork")}>
-			<td className="px-6 py-4 whitespace-nowrap">
-				<div className="flex items-center">
-					<div className="flex-shrink-0 h-10 w-10">
-						<img
-							className="h-10 w-10 rounded-full"
-							// src={person.image}
-							alt=""
-						/>
-					</div>
-					<div className="ml-4">
-						<div className="text-sm font-medium text-gray-900">
-							{artwork.title}
-						</div>
-						<div className="text-sm text-gray-500">{/* {artist.id} */}</div>
-					</div>
-				</div>
-			</td>
-			<td className="px-6 py-4 whitespace-nowrap">
-				<div className="text-sm text-gray-900">{artist.name}</div>
-				<div className="text-sm text-gray-500">{/* {person.department} */}</div>
-			</td>
-			<td className="px-6 py-4 whitespace-nowrap">
-				<span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-					{medium.medium}
-				</span>
-			</td>
-			<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-				{/* {person.role} */}
-			</td>
-		</tr>
-	);
+  return (
+    <tr key={key}>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="flex items-center">
+          <div className="flex-shrink-0 h-10 w-10">
+            <img
+              className="h-10 w-10 rounded-full"
+              // src={person.image}
+              alt=""
+            />
+          </div>
+          <div className="ml-4">
+            <Link
+              to={{
+                pathname: `/artwork`,
+                state: { artwork, artist, medium },
+              }}
+            >
+              <div className="text-sm font-medium text-gray-900">
+                {artwork.title}
+              </div>
+            </Link>
+            <div className="text-sm text-gray-500">{/* {artist.id} */}</div>
+          </div>
+        </div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">{artist.name}</div>
+        <div className="text-sm text-gray-500">{/* {person.department} */}</div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+          {medium.medium}
+        </span>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+        {/* {person.role} */}
+      </td>
+    </tr>
+  );
 };
 
 export default withRouter(ArtworkListView);
