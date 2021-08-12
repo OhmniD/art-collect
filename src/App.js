29251c2daa3d9fe3d9698edc714e5596cfc0782d
OhmniD <dave@ohmnid.com>
2021-08-12 09:50:42 +0100

    adds link to artwork page in main router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route } from "react-router";
 import Navbar from "./components/navigation/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import Artworks from "./components/artworks/Artworks";
-// import Artwork from "./components/artworks/Artwork";
+import Artwork from "./components/artworks/Artwork";
 import { CollectionProvider } from "./providers/CollectionProvider";
 import { ArtworkProvider } from "./providers/ArtworkProvider";
 import { ArtistProvider } from "./providers/ArtistProvider";
@@ -23,10 +23,10 @@ function App() {
 							<main className="py-10 px-10">
 								<Route path="/" exact component={Dashboard} />
 								<Route path="/artworks" component={Artworks} />
-								{/* <Route
+								<Route
 									path="/artwork"
 									render={(props) => <Artwork {...props} />}
-								/> */}
+								/>
 							</main>
 						</CollectionProvider>
 					</ArtworkProvider>
