163a8a468d54b34569e8a0a1808b30a19edbc910
Dave Murray <dave@ohmnid.com>
2021-08-13 19:05:29 +0100

    adds modal to add an artist

---
+++ src/components/artists/AddArtistForm.js
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { API } from "aws-amplify";
+import { createArtist } from "../../graphql/mutations";
+
+const AddArtist = ({ artists, setArtists, open, setOpen, cancelButtonRef }) => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (evt) => {
+    formData[evt.target.id] = evt.target.value;
+    setFormData(formData);
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    const artist = {
+      name: formData.name,
+      artistStartYear: formData.artistStartYear.parseInt,
+      artistEndYear: formData.artistEndYear.parseInt,
+    };
+    const addedData = await API.graphql({
+      query: createArtist,
+      variables: { input: artist },
+    });
+
+    setFormData({});
+    Array.from(document.querySelectorAll("input")).forEach(
+      (input) => (input.value = "")
+    );
+
+    Array.from(document.querySelectorAll("select")).forEach(
+      (input) => (input.value = "default")
+    );
+
+    setArtists([...artists, addedData.data.createArtist]);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name" className="flex text-sm font-medium text-gray-700">
+        Name
+      </label>
+      <div className="mt-1">
+        <input
+          required
+          onChange={handleChange}
+          type="text"
+          name="name"
+          id="name"
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          placeholder="Takashi Murakami"
+        />
+      </div>
+
+      <label
+        htmlFor="artistStartYear"
+        className="flex text-sm font-medium text-gray-700 mt-1"
+      >
+        Artist start year
+      </label>
+      <div className="mt-1">
+        <input
+          required
+          onChange={handleChange}
+          type="number"
+          name="artistStartYear"
+          id="artistStartYear"
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          placeholder="1998"
+        />
+      </div>
+
+      <label
+        htmlFor="artistEndYear"
+        className="flex text-sm font-medium text-gray-700 mt-1"
+      >
+        Artist end year
+      </label>
+      <div className="mt-1">
+        <input
+          required
+          onChange={handleChange}
+          type="number"
+          name="artistEndYear"
+          id="artistEndYear"
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          placeholder="2021"
+        />
+      </div>
+
+      <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+        <button
+          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+          onClick={() => setOpen(false)}
+        >
+          Add artwork
+        </button>
+        <button
+          type="button"
+          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+          onClick={() => setOpen(false)}
+          ref={cancelButtonRef}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddArtist;
--- src/components/artists/Artists.js
+++ src/components/artists/Artists.js
@@ -2,39 +2,46 @@ import { useContext, useState } from "react";
 // import AddArtistModal from "./AddArtistModal";
 import { ArtistsContext } from "../../providers/ArtistProvider";
 import { ArtworksContext } from "../../providers/ArtworkProvider";
+import AddArtistModal from "./AddArtistModal";
 import ArtistCard from "./ArtistCard";
 
 export default function Artists() {
-	const { artists } = useContext(ArtistsContext);
-	const { artworks, setArtworks } = useContext(ArtworksContext);
+  const { artists, setArtists } = useContext(ArtistsContext);
+  const { artworks, setArtworks } = useContext(ArtworksContext);
 
-	const [open, setOpen] = useState(false); //sets state of add artwork modal
+  const [open, setOpen] = useState(false); //sets state of add artist modal
 
-	const artistNodes = artists.map((artist) => {
-		const artistWorks = artworks.filter(
-			(artwork) => artwork.artistID === artist.id
-		);
-		return <ArtistCard artist={artist} artistWorks={artistWorks} />;
-	});
+  const artistNodes = artists.map((artist) => {
+    const artistWorks = artworks.filter(
+      (artwork) => artwork.artistID === artist.id
+    );
+    return <ArtistCard artist={artist} artistWorks={artistWorks} />;
+  });
 
-	return (
-		<div>
-			<div className="flex justify-between">
-				<h1 className="text-3xl font-bold leading-tight text-gray-900">
-					Artists
-				</h1>
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold leading-tight text-gray-900">
+          Artists
+        </h1>
 
-				<button
-					// onClick={() => setOpen(!open)}
-					type="button"
-					className="inline-flex items-center px-4 py-2 mb-5 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-				>
-					Add artist
-				</button>
-			</div>
-			<ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-				{artistNodes}
-			</ul>
-		</div>
-	);
+        <button
+          onClick={() => setOpen(!open)}
+          type="button"
+          className="inline-flex items-center px-4 py-2 mb-5 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Add artist
+        </button>
+      </div>
+      <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {artistNodes}
+      </ul>
+      <AddArtistModal
+        artists={artists}
+        setArtists={setArtists}
+        open={open}
+        setOpen={setOpen}
+      />
+    </div>
+  );
 }
